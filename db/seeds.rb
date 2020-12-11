3de32eba86ec62069406443527e91f4bfd12ba39
github username <github email address>
2020-12-11 13:42:26 -0600

    Want to try and add validations to room number and class titles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+SchoolClass.destroy_all
+
+student1 = Student.create(first_name: "Kirsty", last_name: "Brewster")
+student2 = Student.create(first_name: "Briana", last_name: "Brewster")
+sc1 = SchoolClass.create(title: "Algorithms", room_number: 4)
+sc2 = SchoolClass.create(title: "HTML 101", room_number: 5)
